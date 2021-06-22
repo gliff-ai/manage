8429971b266ad4d5747bcf9f67398a5bc13d8111
Silvia Zeni <silvia.szn@gmail.com>
2021-06-22 15:02:15 +0100

    feat: Apply material-ui to projects

--- src/views/ProjectsView.tsx
+++ src/views/ProjectsView.tsx
@@ -1,7 +1,40 @@
 import { useEffect, useState, ChangeEvent } from "react";
 import { ServiceFunctions } from "@/api";
+import {
+  Paper,
+  IconButton,
+  Grid,
+  Typography,
+  Card,
+  makeStyles,
+  Theme,
+  InputBase,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+} from "@material-ui/core";
+import { Add } from "@material-ui/icons";
 import { useAuth } from "@/hooks/use-auth";
-import { Profile, Project, Team } from "@/interfaces";
+import { Project, Team } from "@/interfaces";
+import { InvitePopover } from "@/components/InvitePopover";
+
+const useStyles = (props: Props) =>
+  makeStyles((theme: Theme) => ({
+    paperHeader: {
+      padding: "10px",
+      backgroundColor: theme.palette.primary.main,
+    },
+    projectsTopography: {
+      color: "#ffffff",
+      display: "inline",
+      fontSize: "21px",
+      marginRight: "125px",
+    },
+    projectAddIconBtn: {
+      right: "16px",
+    },
+  }));
 
 interface Props {
   services: ServiceFunctions;
@@ -15,13 +48,14 @@ export const ProjectsView = (props: Props): JSX.Element => {
   const [projectInvitees, setInvitees] = useState([]);
 
   if (!auth) return null;
+  const classes = useStyles(props)();
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     const { value } = event.target;
     setProjectName(value);
   };
 
-  const handleSelectChange = (event: ChangeEvent<HTMLSelectElement>) => {
+  const handleSelectChange = (event: ChangeEvent<HTMLSelectElement>): void => {
     const { value } = event.target;
     setInvitee(value);
   };
@@ -58,54 +92,58 @@ export const ProjectsView = (props: Props): JSX.Element => {
       });
   };
 
-  const project = ({ name, uid }: Project) => (
-    <li key={name}>
-      <div>{name}</div>
-      <button
-        type="button"
-        onClick={() => inviteToProject(uid, projectInvitee)}
-      >
-        Invite
-      </button>
-    </li>
-  );
+  const createProject = async (): Promise<void> => {
+    await props.services.createProject({ name: projectName });
+    const p = (await props.services.getProjects()) as Project[];
+    setProjects(p);
+    setProjectName("");
+  };
 
-  const inviteSelect = (
-    <select value={projectInvitee} onChange={handleSelectChange}>
-      {projectInvitees.map((el: Profile) => (
-        <option key={el.email} value={el.email}>
-          {el.name}
-        </option>
-      ))}
-    </select>
+  const project = ({ name, uid }: Project) => (
+    <ListItem key={name}>
+      <ListItemText key={name} primary={name} />
+      <ListItemSecondaryAction>
+        <InvitePopover
+          projectInvitee={projectInvitee}
+          projectInvitees={projectInvitees}
+          handleSelectChange={handleSelectChange}
+          inviteToProject={() => inviteToProject(uid, projectInvitee)}
+        />
+      </ListItemSecondaryAction>
+    </ListItem>
   );
 
   return (
-    <>
-      <h1>Projects</h1>
-      <input
-        type="text"
-        name="project-name"
-        onChange={handleChange}
-        value={projectName}
-      />
-      <button
-        type="button"
-        onClick={async () => {
-          await props.services.createProject({ name: projectName });
-          const p = (await props.services.getProjects()) as Project[];
-          setProjects(p);
-          setProjectName("");
-        }}
-      >
-        New Project
-      </button>
-
-      <ul>{projects.map(project)}</ul>
-
-      <h3>Invite user</h3>
-
-      {inviteSelect}
-    </>
+    <Grid container direction="row">
+      <Card>
+        <Paper
+          elevation={0}
+          variant="outlined"
+          square
+          className={classes.paperHeader}
+        >
+          <Typography className={classes.projectsTopography}>
+            Projects
+          </Typography>
+        </Paper>
+        <Paper elevation={0} square>
+          <InputBase
+            placeholder="New project"
+            value={projectName}
+            onChange={handleChange}
+            inputProps={{
+              style: { fontSize: 18, marginLeft: 15, marginRight: 50 },
+            }}
+          />
+          <IconButton
+            onClick={createProject}
+            className={classes.projectAddIconBtn}
+          >
+            <Add />
+          </IconButton>
+          <List>{projects.map(project)}</List>
+        </Paper>
+      </Card>
+    </Grid>
   );
 };
