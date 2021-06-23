26b1e14ece096001910c1fae5b69f1d821409298
Silvia Zeni <silvia.szn@gmail.com>
2021-06-23 09:35:33 +0100

    chore: Apply Material-UI (#64)
    * chore: Add .vscode to .gitignore
    
    * chore: Add material-ui to package.json
    
    * feat: Apply material-ui to projects
    
    * feat: Apply material-ui to users

---
+++ src/components/InvitePopover.tsx
@@ -0,0 +1,106 @@
+import { useState, ChangeEvent } from "react";
+import {
+  Paper,
+  Button,
+  Card,
+  Popover,
+  IconButton,
+  Typography,
+  makeStyles,
+  Theme,
+  TextField,
+  MenuItem,
+} from "@material-ui/core";
+import { PersonAdd } from "@material-ui/icons";
+import { Profile } from "@/interfaces";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  paperHeader: { padding: "10px", backgroundColor: theme.palette.primary.main },
+  card: {
+    display: "flex",
+    flexDirection: "column",
+    width: "auto",
+    hegith: "400px",
+  },
+  inviteBtn: {
+    marginLeft: "15px",
+  },
+  userInviteTopography: {
+    color: "#ffffff",
+    display: "inline",
+    fontSize: "21px",
+    marginRight: "125px",
+  },
+  paperBody: {
+    margin: "15px",
+  },
+}));
+
+interface Props {
+  projectInvitee: string;
+  projectInvitees: Profile[];
+  handleSelectChange: (event: ChangeEvent<HTMLSelectElement>) => void;
+  inviteToProject: () => void;
+}
+
+export function InvitePopover(props: Props): React.ReactElement {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const inviteSelect = (
+    <form noValidate autoComplete="off">
+      <TextField
+        select
+        value={props.projectInvitee}
+        onChange={() => props.handleSelectChange}
+      >
+        {props.projectInvitees.map((el: Profile) => (
+          <MenuItem key={el.email} value={el.email}>
+            {el.name}
+          </MenuItem>
+        ))}
+      </TextField>
+      <Button className={classes.inviteBtn} onClick={props.inviteToProject}>
+        Invite
+      </Button>
+    </form>
+  );
+
+  return (
+    <>
+      <IconButton onClick={handleClick}>
+        <PersonAdd />
+      </IconButton>
+      <Popover
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+      >
+        <Card className={classes.card}>
+          <Paper
+            elevation={0}
+            variant="outlined"
+            square
+            className={classes.paperHeader}
+          >
+            <Typography className={classes.userInviteTopography}>
+              Invite User
+            </Typography>
+          </Paper>
+          <Paper elevation={0} square className={classes.paperBody}>
+            {inviteSelect}
+          </Paper>
+        </Card>
+      </Popover>
+    </>
+  );
+}
--- src/views/UsersView.tsx
+++ src/views/UsersView.tsx
@@ -1,8 +1,42 @@
 import { ChangeEvent, useEffect, useState } from "react";
-
+import {
+  Paper,
+  IconButton,
+  Grid,
+  Typography,
+  Card,
+  makeStyles,
+  Theme,
+  List,
+  ListItem,
+  TextField,
+} from "@material-ui/core";
+import { Send } from "@material-ui/icons";
 import { Team } from "@/interfaces";
 import { ServiceFunctions } from "@/api";
 import { useAuth } from "@/hooks/use-auth";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  topography: {
+    color: "#ffffff",
+    display: "inline",
+    fontSize: "21px",
+    marginRight: "125px",
+  },
+  paperHeader: {
+    padding: "10px",
+    backgroundColor: theme.palette.primary.main,
+  },
+  paperBody: {
+    margin: "15px",
+    width: "400px",
+    height: "auto",
+    fontSize: "17px",
+  },
+  pendingInvitesCard: {
+    marginTop: "20px",
+  },
+}));
 
 interface Props {
   services: ServiceFunctions;
@@ -11,18 +45,34 @@ interface Props {
 export const UsersView = (props: Props): JSX.Element => {
   const auth = useAuth();
 
+  if (!auth) return null;
   const [team, setTeam] = useState<Team>({
     profiles: [],
     pending_invites: [],
   });
   const [inviteEmail, setInviteEmail] = useState("");
   const [inviteMessage, setInviteMessage] = useState("");
+  const classes = useStyles();
 
-  if (!auth) return null;
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     const { value } = event.target;
     setInviteEmail(value);
+  };
+
+  const inviteNewUser = async (event: ChangeEvent): Promise<void> => {
+    event.preventDefault();
+    setInviteMessage("");
+
+    const result = await props.services.inviteUser({
+      email: inviteEmail,
+    });
+
+    if (result) {
+      setInviteEmail("");
+      setInviteMessage("Invite was sent");
+    } else {
+      setInviteMessage("An error happened with the invite");
+    }
   };
 
   useEffect(() => {
@@ -36,61 +86,75 @@ export const UsersView = (props: Props): JSX.Element => {
   let pendingInvites;
   if (team?.pending_invites?.length > 0) {
     pendingInvites = (
-      <ul>
+      <List>
         {team?.pending_invites.map(({ email, sent_date }) => (
-          <li key={email}>
+          <ListItem key={email}>
             {email} - {sent_date}
-          </li>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     );
   } else {
-    pendingInvites = <>No Pending Invites</>;
+    pendingInvites = <>No pending invites</>;
   }
+
+  const inviteForm = (
+    <form autoComplete="off" onSubmit={() => inviteNewUser}>
+      <div>{inviteMessage}</div>
+      <TextField
+        id="invite-email"
+        type="email"
+        required
+        onChange={handleChange}
+        value={inviteEmail}
+      />
+      <IconButton type="submit" onSubmit={(e) => e.preventDefault()}>
+        <Send />
+      </IconButton>
+    </form>
+  );
 
   return (
     <>
-      <h1>Current Users</h1>
-      <ul>
-        {team?.profiles.map(({ email, name }) => (
-          <li key={email}>{name}</li>
-        ))}
-      </ul>
-
-      <h1>Pending Invites</h1>
-      {pendingInvites}
-
-      {/*  TODO make component */}
-      <form
-        onSubmit={async (event) => {
-          event.preventDefault();
-          setInviteMessage("");
-
-          const result = await props.services.inviteUser({
-            email: inviteEmail,
-          });
-
-          if (result) {
-            setInviteEmail("");
-            setInviteMessage("Invite was sent");
-          } else {
-            setInviteMessage("An error happened with the invite");
-          }
-        }}
-      >
-        <div>{inviteMessage}</div>
-        <label htmlFor="invite-email">
-          Invite new user:&nbsp;
-          <input
-            name="invite-email"
-            type="email"
-            onChange={handleChange}
-            value={inviteEmail}
-            required
-          />
-          <input type="submit" value="Invite" />
-        </label>
-      </form>
+      <Grid container direction="row">
+        <Card>
+          <Paper
+            elevation={0}
+            variant="outlined"
+            square
+            className={classes.paperHeader}
+          >
+            <Typography className={classes.topography}>
+              Current Users
+            </Typography>
+          </Paper>
+          <Paper elevation={0} square className={classes.paperBody}>
+            <List>
+              {team?.profiles.map(({ email, name }) => (
+                <ListItem key={email}>{name}</ListItem>
+              ))}
+            </List>
+          </Paper>
+        </Card>
+      </Grid>
+      <Grid container direction="row">
+        <Card className={classes.pendingInvitesCard}>
+          <Paper
+            elevation={0}
+            variant="outlined"
+            square
+            className={classes.paperHeader}
+          >
+            <Typography className={classes.topography}>
+              Pending Invites
+            </Typography>
+          </Paper>
+          <Paper elevation={0} square className={classes.paperBody}>
+            {pendingInvites}
+            {inviteForm}
+          </Paper>
+        </Card>
+      </Grid>
     </>
   );
 };
