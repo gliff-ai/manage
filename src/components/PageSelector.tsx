b57c4f1ea25e45dd41ad6d7207dbeb270da5179d
Craig Cooper <c@c9r.dev>
2021-09-10 16:07:29 +0000

    feat: tidy page selector

--- src/components/PageSelector.tsx
+++ src/components/PageSelector.tsx
@@ -1,54 +1,31 @@
 import { ButtonGroup } from "@material-ui/core";
 import { ReactElement } from "react";
-import { useNavigate } from "react-router-dom";
-import { BaseIconButton } from "@gliff-ai/style";
-import { imgSrc } from "@/helpers"
+import { Link } from "react-router-dom";
+import { IconButton } from "@gliff-ai/style";
+import { imgSrc } from "@/helpers";
 
-const tooltips = {
-  projects: {
-    name: "Projects",
-    icon: imgSrc("projects"),
-  },
-  team: {
-    name: "Team members",
-    icon: imgSrc("team"),
-  },
-  collaborators: {
-    name: "External collaborators",
-    icon: imgSrc("team"),
-  },
-};
+const links = ["Projects", "Team", "Collaborators"] as const;
 
 interface Props {
-  page: "team" | "projects" | "collaborators";
+  page: Lowercase<typeof links[number]>;
 }
 
-export function PageSelector(props: Props): ReactElement {
-  const navigate = useNavigate();
-  // TODO use Link here to wrap the buttons
+export function PageSelector({ page }: Props): ReactElement {
   return (
-    <ButtonGroup>
-      <BaseIconButton
-        tooltip={tooltips.projects}
-        fill={props.page === "projects"}
-        onClick={() => {
-          navigate("../projects");
-        }}
-      />
-      <BaseIconButton
-        tooltip={tooltips.team}
-        fill={props.page === "team"}
-        onClick={() => {
-          navigate("../team");
-        }}
-      />
-      <BaseIconButton
-        tooltip={tooltips.collaborators}
-        fill={props.page === "collaborators"}
-        onClick={() => {
-          navigate("../collaborators");
-        }}
-      />
+    <ButtonGroup orientation="vertical">
+      {links.map((name) => {
+        const link = name.toLowerCase();
+
+        return (
+          <IconButton
+            component={Link}
+            to={link}
+            tooltip={{ name }}
+            icon={imgSrc(link)}
+            fill={page === link}
+          />
+        );
+      })}
     </ButtonGroup>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,9 @@ export default defineConfig({
       },
     },
   },
+  optimizeDeps: {
+    exclude: ["@gliff-ai/style"]
+  },
   esbuild: {
     jsxInject: `import React from 'react'`,
   },
