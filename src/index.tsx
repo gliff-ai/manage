accbf247b0331d6cf122550708c1e78c048c5441
Craig Cooper <c@c9r.dev>
2021-09-14 10:52:22 +0000

    chore: lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import {
   AppBar,
   CssBaseline,
