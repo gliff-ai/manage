8b72197501cc56be0177b6feb70e327b3edfe44f
Craig Cooper <c@c9r.dev>
2021-11-12 16:46:56 +0000

    fix: add run script to package

--- __tests__/e2e/index.test.js
+++ __tests__/e2e/index.test.js
@@ -1,9 +1,9 @@
 const { wrapper, test, webdriver } =
-  require("@gliff-ai/jest-browserstack-automate")();
+  require("@gliff-ai/jest-browserstack-automate")("Manage");
 
 const { TARGET_URL = "http://127.0.0.1:3000/" } = process.env;
 
-wrapper("Manage", () => {
+wrapper(() => {
   describe("Load page", () => {
     test("loads the page", async (driver) => {
       await driver.get(TARGET_URL);
--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -12,11 +12,7 @@ module.exports = {
   // setupFilesAfterEnv: ["./jest-setup.ts"],
 
   // The glob patterns Jest uses to detect test files
-  testMatch: [
-    "**/__tests__/*.[jt]s?(x)",
-    "**/?(*.)+(spec|test).[tj]s?(x)",
-    "!**/__tests__/e2e/*",
-  ],
+  testMatch: ["**/__tests__/e2e/*"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: ["/node_modules/", "dist/"],
