09eb300fe732df7aa27b7e675f66e381c908cb0d
Craig Cooper <c@c9r.dev>
2021-11-12 16:07:43 +0000

    fix: use new wrapper

---
+++ __tests__/e2e/index.test.js
@@ -0,0 +1,17 @@
+const { wrapper, test, webdriver } =
+  require("@gliff-ai/jest-browserstack-automate")();
+
+const { TARGET_URL = "http://127.0.0.1:3000/" } = process.env;
+
+wrapper("Manage", () => {
+  describe("Load page", () => {
+    test("loads the page", async (driver) => {
+      await driver.get(TARGET_URL);
+
+      await driver.wait(webdriver.until.titleMatches(/Manage/i), 60000);
+      const title = await driver.getTitle();
+
+      expect(title).toEqual("gliff.ai MANAGE");
+    });
+  });
+});
