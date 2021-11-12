0ee3be9cb5c2e79fe6ed40239f82e7a3c9ed00b7
Craig Cooper <c@c9r.dev>
2021-11-12 08:35:45 +0000

    test: multibrowser tests

--- test/e2e/index.test.js
+++ test/e2e/index.test.js
@@ -1,29 +1,28 @@
-const {
-  wrapper,
-  test,
-  webdriver,
-} = require("@gliff-ai/jest-browserstack-automate");
+const { wrapper, test, webdriver } =
+  require("@gliff-ai/jest-browserstack-automate")();
 
-const { TARGET_URL = "http://localhost:3000/" } = process.env;
+const { TARGET_URL = "http://127.0.0.1:3000/" } = process.env;
+
+const timeout = 60000;
+
+const capabilities = {
+  project: "Manage",
+};
 
 wrapper(() => {
   describe("Load page", () => {
-    test("loads the page", async (driver) => {
-      await driver.get(TARGET_URL);
+    test(
+      "loads the page",
+      async (driver) => {
+        await driver.get(TARGET_URL);
 
-      await driver.wait(webdriver.until.titleMatches(/Manage/i), 5000);
-      const title = await driver.getTitle();
+        await driver.wait(webdriver.until.titleMatches(/Manage/i), 60000);
+        const title = await driver.getTitle();
 
-      expect(title).toEqual("gliff.ai MANAGE");
-    });
-    //
-    // testIt("loads the page 2", async (driver) => {
-    //   await driver.get(TARGET_URL);
-    //
-    //   await driver.wait(webdriver.until.titleMatches(/Manage/i), 5000);
-    //   const title = await driver.getTitle();
-    //
-    //   expect(title).toEqual("gliff.ai MANAGEddd");
-    // });
+        expect(title).toEqual("gliff.ai MANAGE");
+      },
+      timeout,
+      { capabilities }
+    );
   });
 });
