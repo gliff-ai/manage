308c24350bbd77accc3c3349e1b3c4a11e3d0889
Silvia Zeni <silvia.szn@gmail.com>
2021-12-13 18:42:25 +0000

    feat: Add ProgressBar to components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { LaunchIcon } from "./LaunchIcon";
 export { PageSelector } from "./PageSelector";
 export { InviteDialog } from "./InviteDialog";
 export { CreateProjectDialog } from "./CreateProjectDialog";
+export { ProgressBar } from "./ProgressBar";
