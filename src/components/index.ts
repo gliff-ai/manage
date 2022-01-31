32b8d6cb0cae33e3a392264a078bd0e62ca7fa2c
Silvia Zeni <silvia.szn@gmail.com>
2022-01-31 17:03:35 +0000

    chore: Export plugin components

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,4 +4,6 @@ export { EditProjectDialog } from "./EditProjectDialog";
 export { CreateProjectDialog } from "./CreateProjectDialog";
 export { ProgressBar } from "./ProgressBar";
 export { AddPluginDialog } from "@/components/plugins/AddPluginDialog";
+export { EditPluginDialog } from "@/components/plugins/EditPluginDialog";
+export { DeletePluginDialog } from "@/components/plugins/DeletePluginDialog";
 export { MessageAlert } from "./MessageAlert";
