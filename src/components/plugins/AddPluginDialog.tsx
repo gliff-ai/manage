a6b12bc15c9757cc4d171ad8f8377396b9aaf122
Silvia Zeni <silvia.szn@gmail.com>
2022-02-09 17:21:34 +0000

    fix: Simplify regular expression

--- src/components/plugins/AddPluginDialog.tsx
+++ src/components/plugins/AddPluginDialog.tsx
@@ -190,16 +190,8 @@ export function AddPluginDialog({
   );
 
   const isValidURL = (url: string): boolean => {
-    const pattern = new RegExp(
-      "^(https?:\\/\\/)?" + // protocol
-        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-        "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-        "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-        "(\\#[-a-z\\d_]*)?$",
-      "i"
-    ); // fragment locator
-    return !!pattern.test(url);
+    const pattern = new RegExp("^https?:\\/\\/");
+    return pattern.test(url);
   };
 
   const enterValuesDialog = dialogPage === DialogPage.enterValues && (
