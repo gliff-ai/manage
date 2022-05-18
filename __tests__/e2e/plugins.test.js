e32b99e3d4dab0d00a687b54da375536bf01dc96
Craig Cooper <craig@gliff.ai>
2022-05-18 15:36:35 +0800

    v broken WIP

--- __tests__/e2e/plugins.test.js
+++ __tests__/e2e/plugins.test.js
@@ -27,96 +27,81 @@ wrapper(() => {
 
       const text = "Plugins";
 
-      // sleep(1000)
-
-      console.log("go!")
-      const x = driver.findElement(By.xpath(`//p[text()='Plugins']`))
-
-      console.log(x)
-
       const targetEl = await findElementByText(driver, text, "p");
-
-      // sleep(1000)
-
-      console.log(targetEl);
 
       const res = targetEl.getText()
 
-      console.log(res);
+      expect(res).toEqual(text);
 
-      // expect(res).toEqual(text);
-
-      console.log("doing percy");
       await percySnapshot(driver, "Plugins Page");
-      console.log("done percy");
     }, 30000, {capabilities: {"browserstack.idleTimeout": 300}});
 
-    // test("add plugin dialog", async (driver, percySnapshot) => {
-    //   await openApp(driver, webdriver, TARGET_URL);
-    //
-    //   const el1 = await findElementById(driver, "plugins");
-    //
-    //   await el1.click();
-    //
-    //   const el2 = await findElementById(driver, "add-plugin");
-    //
-    //   await el2.click();
-    //
-    //   await sleep();
-    //
-    //   const text = "Add Plug-in";
-    //
-    //   const targetEl = await findElementByText(driver, text, "p");
-    //
-    //   expect(await targetEl.getText()).toEqual(text);
-    //
-    //   await percySnapshot(driver, "Add Plugin - Dialog 1");
-    //
-    //   const el3 = await findElementByText(driver, "Continue", "button");
-    //
-    //   await el3.click();
-    //
-    //   await percySnapshot(driver, "Add Plugin - Dialog 2");
-    // });
-    //
-    // test("edit plugin dialog", async (driver, percySnapshot) => {
-    //   await openApp(driver, webdriver, TARGET_URL);
-    //
-    //   const el1 = await findElementById(driver, "plugins");
-    //
-    //   await el1.click();
-    //
-    //   await clickHiddenElement(driver, "edit-plugin-python-plugin");
-    //
-    //   await sleep();
-    //
-    //   const text = "Edit Plug-in";
-    //
-    //   const targetEl = await findElementByText(driver, text, "p");
-    //
-    //   expect(await targetEl.getText()).toEqual(text);
-    //
-    //   await percySnapshot(driver, "Edit Plugin Dialog");
-    // });
-    //
-    // test("delete plugin dialog", async (driver, percySnapshot) => {
-    //   await openApp(driver, webdriver, TARGET_URL);
-    //
-    //   const el1 = await findElementById(driver, "plugins");
-    //
-    //   await el1.click();
-    //
-    //   await clickHiddenElement(driver, "delete-plugin-python-plugin");
-    //
-    //   await sleep();
-    //
-    //   const text = "Are You Sure?";
-    //
-    //   const targetEl = await findElementByText(driver, text, "p");
-    //
-    //   expect(await targetEl.getText()).toEqual(text);
-    //
-    //   await percySnapshot(driver, "Delete Plugin Dialog");
-    // });
-  }, 5000);
+    test("add plugin dialog", async (driver, percySnapshot) => {
+      await openApp(driver, webdriver, TARGET_URL);
+
+      const el1 = await findElementById(driver, "plugins");
+
+      await el1.click();
+
+      const el2 = await findElementById(driver, "add-plugin");
+
+      await el2.click();
+
+      await sleep();
+
+      const text = "Add Plug-in";
+
+      const targetEl = await findElementByText(driver, text, "p");
+
+      expect(await targetEl.getText()).toEqual(text);
+
+      await percySnapshot(driver, "Add Plugin - Dialog 1");
+
+      const el3 = await findElementByText(driver, "Continue", "button");
+
+      await el3.click();
+
+      await percySnapshot(driver, "Add Plugin - Dialog 2");
+    });
+
+    test("edit plugin dialog", async (driver, percySnapshot) => {
+      await openApp(driver, webdriver, TARGET_URL);
+
+      const el1 = await findElementById(driver, "plugins");
+
+      await el1.click();
+
+      await clickHiddenElement(driver, "edit-plugin-python-plugin");
+
+      await sleep();
+
+      const text = "Edit Plug-in";
+
+      const targetEl = await findElementByText(driver, text, "p");
+
+      expect(await targetEl.getText()).toEqual(text);
+
+      await percySnapshot(driver, "Edit Plugin Dialog");
+    });
+
+    test("delete plugin dialog", async (driver, percySnapshot) => {
+      await openApp(driver, webdriver, TARGET_URL);
+
+      const el1 = await findElementById(driver, "plugins");
+
+      await el1.click();
+
+      await clickHiddenElement(driver, "delete-plugin-python-plugin");
+
+      await sleep();
+
+      const text = "Are You Sure?";
+
+      const targetEl = await findElementByText(driver, text, "p");
+
+      expect(await targetEl.getText()).toEqual(text);
+
+      await percySnapshot(driver, "Delete Plugin Dialog");
+    });
+  });
 });
